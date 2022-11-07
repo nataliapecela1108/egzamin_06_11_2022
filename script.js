076f0087f8368c15ccd5b9ec79a2be1103491bbc
nataliapecela1108 <116489503+nataliapecela1108@users.noreply.github.com>
2022-11-07 21:43:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const showH1 = () => {
+    const title = document.getElementById("namePo");
+    title.textContent = "Natalia";
+}
+
+showH1();
+
+
+const showH2 = () => {
+    const btn = document.getElementById("lastName");
+    btn.textContent = "Pecela";
+}
+
+showH2();
+
+
